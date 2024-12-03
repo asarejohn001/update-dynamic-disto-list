3abf946babf6177959ff3e19f30a97d426cbe3d4
asarejohn001 <asarejohn001@gmail.com>
2024-12-03 15:02:48 -0500

    Install and import module

--- update-dynamic-distroList-members.ps1
+++ update-dynamic-distroList-members.ps1
@@ -1,3 +1,13 @@
 <#
-Autho
+Author: John Asare
+Date: 2024/12/03
+
+Description: Update the dynamic group members
 #>
+
+# Install module if not already
+Install-Module ExchangeOnlineManagement
+
+# Import module
+Import-Module ExchangeOnlineManagement
+
