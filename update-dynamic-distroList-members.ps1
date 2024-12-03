6d4f34d961e587e3dfc671cbb790b5612967c939
asarejohn001 <asarejohn001@gmail.com>
2024-12-03 15:08:09 -0500

    add function to record logs

--- update-dynamic-distroList-members.ps1
+++ update-dynamic-distroList-members.ps1
@@ -11,3 +11,16 @@ Install-Module ExchangeOnlineManagement
 # Import module
 Import-Module ExchangeOnlineManagement
 
+# Function to log warnings, errors, and success message
+function Get-Log {
+    param (
+        [string]$LogFilePath,
+        [string]$LogMessage
+    )
+
+    # Create the log entry with the current date and time
+    $logEntry = "{0} - {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $LogMessage
+
+    # Append the log entry to the log file
+    Add-Content -Path $LogFilePath -Value $logEntry
+}
