01378fc69ae395dc745e4a05d4afdad4b3f03887
asarejohn001 <asarejohn001@gmail.com>
2024-12-03 16:05:59 -0500

    update the script to first filter the dynamic rule and then append the new rule

--- update-dynamic-distroList-members.ps1
+++ update-dynamic-distroList-members.ps1
@@ -6,7 +6,7 @@ Description: Update the dynamic group members
 #>
 
 # Install module if not already
-Install-Module ExchangeOnlineManagement
+#Install-Module ExchangeOnlineManagement
 
 # Import module
 Import-Module ExchangeOnlineManagement
@@ -27,7 +27,7 @@ function Get-Log {
 
 # Set variables
 $logFilePath = ".\log.txt"
-$dynamicGroupName = "dph_all@dph.sc.gov"
+$dynamicGroupName = "DHEC_All@dhec.sc.gov"
 
 # Connect to EAC
 try {
@@ -39,27 +39,25 @@ catch {
     Exit
 }
 
-# Get the existing filter of the dynamic distribution group
+# Fetch the current recipient filter
+$currentFilter = (Get-DynamicDistributionGroup -Identity $dynamicGroupName).RecipientFilter
+
+# Append the new exclusion condition
+$newCondition = " -and (-not(PrimarySmtpAddress -eq 'securityalerts@dhec.sc.gov'))"
+$updatedFilter = "($currentFilter)$newCondition"
+
+# Apply the updated filter
 try {
-    $dynamicGroup = Get-DynamicDistributionGroup -Identity $dynamicGroupName
-    $currentFilter = $dynamicGroup.RecipientFilter
-
-    # Log current filter
-    Get-Log -LogFilePath $logFilePath -LogMessage "Current filter: $currentFilter"
-
-    # Update the filter to exclude the specific email address
-    $newCondition = " -and (-not(PrimarySmtpAddress -eq 'securityalerts@dhec.sc.gov'))"
-
-    # Update the dynamic distribution group's filter
-    Set-DynamicDistributionGroup -Identity $dynamicGroupName -RecipientFilter $newCondition
-    $newUpdatedFilter = $dynamicGroup.RecipientFilter
+    Set-DynamicDistributionGroup -Identity $dynamicGroupName -RecipientFilter $updatedFilter
 
     # Log success
-    Get-Log -LogFilePath $logFilePath -LogMessage "Updated filter: $newUpdatedFilter"
+    Get-Log -LogFilePath $logFilePath -LogMessage "Successfully appended exclusion condition. New filter: $updatedFilter"
 }
 catch {
-    Get-Log -LogFilePath $logFilePath -LogMessage "Error updating dynamic distribution group: $_"
+    Get-Log -LogFilePath $logFilePath -LogMessage "Error updating the filter: $_"
 }
+
+
 finally {
     #Disconnect-ExchangeOnline -Confirm:$false
     #Get-Log -LogFilePath $logFilePath -LogMessage "Disconnected from Exchange Online."
